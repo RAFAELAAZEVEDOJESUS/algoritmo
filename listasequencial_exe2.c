c1fb78282e8cd3a9e31c68d417884f71c3019f04
rafaellahaapy <rafaellahaapy@gmail.com>
2022-11-06 09:26:41 -0300

    sequencial

---
+++ listasequencial_exe2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	
+	
+	
+	printf("\nCaderno de poesias e um belo lugar.");
+	printf("\nTantas coisas lindas que eu gostaria de falar");
+	printf("\nEu falo em forma de versos para todos poderem escutar");
+	printf("\nAgora voce ja sabe por que os poetas passam os dias escrevendo em seus \ncadernos de poemas.");
+
+	return 0;
+		
+}
---
+++ listasequencial_exe6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main (){
+	int A,B,X;
+	
+	A=10;
+	B=10;
+	X=A+B;
+	
+	printf("O valor de A + B eh: %d",X);
+	return 0;
+}
