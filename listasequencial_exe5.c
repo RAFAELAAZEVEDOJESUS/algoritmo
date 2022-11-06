ea11b1eb7f7538863ea0cf08334821846de083f8
rafaellahaapy <rafaellahaapy@gmail.com>
2022-11-06 09:37:30 -0300

    corrigindo seq

---
+++ listasequencial_exe5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+	
+	int main(){
+		
+		int A,B,X;
+		
+		printf("Digite o valor de A:");
+		scanf("%d",&A);
+		
+		printf("Digite o valor de B:");
+		scanf("%d",&B);
+		
+		X=A+B;
+		
+		printf("O valor de A + B e %d:",X);
+		
+	}
+	
+	return 0;
+}
