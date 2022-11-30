7babad6bb275880daa18bdadce3f4ce00a02c6c2
rafaellahaapy <rafaellahaapy@gmail.com>
2022-11-30 12:01:37 -0300

    lista5

---
+++ lista5-exe1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/* Elabore um algoritmo que receba o nome completo de um cliente e imprima a quantidade 
+de caracteres digitados. */
+
+int main(){
+    char nomeCompleto[30];
+
+    printf("Digite seu nome completo: ");
+  //  scanf("%s", &nomeCompleto);
+    fgets(nomeCompleto, 30, stdin);
+
+    printf("\nNome Completo: %s\nQuantidade de caracteres: %d", nomeCompleto, strlen(nomeCompleto) - 1);
+
+
+}
